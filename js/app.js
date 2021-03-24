7f8b9009620b1af743aaea39af46779a17843bf1
Eze Micheal <investormicheal@gmail.com>
2021-03-24 08:26:49 -0700

    Made some more responsive adjustments

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 // Code to toggle the side panel on mobile
 
+let ourProfileBtn = document.querySelector('#user-btn');
+
 let ourMenuBtn = document.querySelector('#menu-btn');
 
 let profileInfoPanel = document.querySelector('#profile-info');
 
 let closePanelBtn = document.querySelector('#close-btn');
 
+let menuBox = document.querySelector('#links');
+
 let isOpen = false;
 
-function showPanel () {
+function toggleDisplay (oldClass, newClass, element) {
     if (isOpen === false) {
-        profileInfoPanel.className = "closed";
+        element.className = oldClass;
         isOpen = true;
     } else {
-        profileInfoPanel.className = "open";
+        element.className = newClass;
         isOpen = false;
     }
 }
 
-window.addEventListener('load', showPanel);
-ourMenuBtn.addEventListener('click', showPanel);
-closePanelBtn.addEventListener('click', showPanel);
+window.addEventListener('load', function () {
+    toggleDisplay('closed', 'open', profileInfoPanel)
+});
+ourProfileBtn.addEventListener('click', function () {
+    toggleDisplay('closed', 'open', profileInfoPanel)
+});
+closePanelBtn.addEventListener('click', function () {
+    toggleDisplay('closed', 'open', profileInfoPanel)
+});
+ourMenuBtn.addEventListener('click', function () {
+    toggleDisplay('hide-menu', 'show-menu', menuBox);
+    console.log('menu open');
+})
